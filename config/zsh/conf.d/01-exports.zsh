d2fd7cc9ac88e6b5f551eae2261f8ea1e1f74665
GMadorell <gmadorell@gmail.com>
2026-07-13 12:52:03 +0200

    zsh: create conf.d/01-exports.zsh

---
+++ config/zsh/conf.d/01-exports.zsh
@@ -0,0 +1,15 @@
+#!/bin/zsh
+# Exports: PATH, GOPATH, MANPATH, PROJECTS_PATH, DOTFILES_PATH
+# Order: early (no dependencies, referenced by later modules like 02-brew)
+
+export GOPATH="$HOME/golang_workspace"
+export PATH="/usr/local/sbin:/usr/local/bin:bin:/usr/sbin:/sbin:$HOME/bin:/usr/bin:$GOPATH/bin:$HOME/.cargo/bin:$PATH"
+export PATH="$HOME/anaconda/bin:$PATH"
+export PATH="$HOME/miniconda3/bin:$PATH"
+export PATH="$HOME/.rbenv/shims:$PATH"
+export PATH="$HOME/.rbenv/bin:$PATH"
+export PATH="$HOME/.local/bin:$PATH"
+export PATH="$(brew --prefix rustup)/bin:$PATH"
+export DOTFILES_PATH="$HOME/.dotfiles"
+export MANPATH="/usr/local/man:$MANPATH"
+export PROJECTS_PATH="$HOME/projects"
