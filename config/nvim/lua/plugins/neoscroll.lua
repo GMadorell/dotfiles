005806722ebb0ecee1f3bd7246aba79c9ff57785
GMadorell <gmadorell@gmail.com>
2026-07-14 01:37:20 +0200

    nvim: neoscroll keep line centered

--- config/nvim/lua/plugins/neoscroll.lua
+++ config/nvim/lua/plugins/neoscroll.lua
@@ -1,6 +1,28 @@
 return {
 	"karb94/neoscroll.nvim",
-	opts = {
-		duration_multiplier = 0.5,
-	},
+	config = function()
+		local neoscroll = require("neoscroll")
+		neoscroll.setup({
+			duration_multiplier = 0.5,
+			-- <C-d>/<C-u> get custom mappings below so they can recenter after scrolling
+			mappings = { "<C-b>", "<C-f>", "<C-y>", "<C-e>", "zt", "zz", "zb" },
+			post_hook = function(info)
+				if info == "center" then
+					vim.cmd("normal! zz")
+				end
+			end,
+		})
+
+		local keymap = {
+			["<C-d>"] = function()
+				neoscroll.ctrl_d({ duration = 250, info = "center" })
+			end,
+			["<C-u>"] = function()
+				neoscroll.ctrl_u({ duration = 250, info = "center" })
+			end,
+		}
+		for key, func in pairs(keymap) do
+			vim.keymap.set({ "n", "v", "x" }, key, func)
+		end
+	end,
 }
