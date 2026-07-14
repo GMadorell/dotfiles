e7078c5c49b1f6a1a16fdb53c5507776e1037020
GMadorell <gmadorell@gmail.com>
2026-07-14 16:25:12 +0200

    nvim: remove gr, use grr

--- config/nvim/lua/plugins/lsp.lua
+++ config/nvim/lua/plugins/lsp.lua
@@ -4,7 +4,6 @@ vim.api.nvim_create_autocmd("LspAttach", {
     local bufopts = { noremap = true, silent = true, buffer = args.buf }
     vim.keymap.set("n", "gd", vim.lsp.buf.definition, bufopts)
     vim.keymap.set("n", "gD", vim.lsp.buf.declaration, bufopts)
-    vim.keymap.set("n", "gr", vim.lsp.buf.references, bufopts)
     vim.keymap.set("n", "gi", vim.lsp.buf.implementation, bufopts)
     vim.keymap.set("n", "K", vim.lsp.buf.hover, bufopts)
     vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, bufopts)
