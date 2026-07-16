5e365dde94b253f03f9108df2b53de5b8288a313
GMadorell <gmadorell@gmail.com>
2026-07-17 01:00:02 +0200

    neovim: add leader fc for current buffer fuzzy find and updated lazy lock

--- config/nvim/lua/plugins/telescope.lua
+++ config/nvim/lua/plugins/telescope.lua
@@ -24,5 +24,6 @@ return {
     vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
     vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
     vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
+    vim.keymap.set('n', '<leader>fc', builtin.current_buffer_fuzzy_find, { desc = 'Telescope fuzzy find in current buffer' })
   end
 }
